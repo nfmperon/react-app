baf5c87e62e94fecf285da58840ef137f2eecbb9
Nicolas Peron <nperon@gmail.com>
2022-08-06 21:42:28 +0200

    feat: add semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/nfmperon/react-app",
+    plugins: [
+        "@semantic-release/commit-analyzer", 
+        "@semantic-release/release-notes-generator", 
+        "@semantic-release/github"
+    ]
+}
