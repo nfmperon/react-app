966bcbec2948b676714973e9cf60cc69f7a2a403
Nicolas Peron <nperon@gmail.com>
2022-08-06 22:46:51 +0200

    feat: update displayed content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Modified on Aug 6
+          Edit <code>src/App.js</code> and save to reload. Modified and modified again on Aug 6.
         </p>
         <a
           className="App-link"
