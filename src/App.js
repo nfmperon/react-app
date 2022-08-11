8e378a573ecd7cfd7c6c088275c7e9e16d4d70fd
Nicolas Peron <nperon@gmail.com>
2022-08-11 16:47:32 +0200

    feat: calculate fibo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+const fibonacci = require ('fibonacci');
+const bigNumber = fibonacci.iterate (8);
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {bigNumber}
         </a>
       </header>
     </div>
